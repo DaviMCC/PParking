4f12c9f65af4f42413d56e7092ce769fda6b03ee
Alex Torres <alextorres7736@gmail.com>
2021-10-06 22:09:12 -0300

    desenvolvimento javascript

---
+++ PParking Project/JS/entrada.js
@@ -0,0 +1,18 @@
+function gerar_ticket()
+{
+    const InsercaoDeDados = document.querySelector(".div-insercao-dados")
+    InsercaoDeDados.style.display= "none"
+    
+    const InstrucaoPlaca = document.querySelector("#mensagem-instrucao")
+    InstrucaoPlaca.style.display = "none"
+
+    const Informacoes = document.querySelector(".p1")
+    Informacoes.style.display = "inline-block";
+  
+    //alterar tamanho da p2 
+    //alterar mensagem da caixa de instrução
+    //Fazer a p2 aparecer
+
+    return false;
+}
+
