4f6180ceac92683f36546e5efef700149ebce227
Davi Carboni <davicarlosm.carboni@gmail.com>
2021-10-20 00:01:00 -0300

    Timeout e function para exibir mensagem de erro

--- PParking Project/JS/pagamento.js
+++ PParking Project/JS/pagamento.js
@@ -40,4 +40,40 @@ function pagar()
     const p2 = document.querySelector('.p4');
     p2.style.display = "inline-block";
 
+    retornarHome();
+}
+
+function alertarErro(mensagem, fase){
+
+
+    if(fase == "checkout"){
+        
+        const p1 = document.querySelector('.p1 form');
+        p1.style.display = "none";
+
+    }else if(fase == "prosseguir"){
+
+        const p2 = document.querySelector('.p2');
+        p2.style.display = "none";
+    }else if(fase == "pagamento"){
+
+        const p3 = document.querySelector('.p3');
+        p3.style.display = "none";
+    }
+  
+    document.getElementsByClassName("titulo")[0].innerHTML = "Ops! Algo não deu certo!";
+
+    document.getElementsByClassName("instrucao")[0].innerHTML = mensagem;
+
+    const instrucao = document.querySelector('.instrucao');
+    instrucao.style.paddingTop = '160px';
+    instrucao.style.fontSize = '45px';
+
+    retornarHome();
+}
+
+function retornarHome(){
+
+    setTimeout((() => {window.location.replace("cabine_pagamentos.html")}), 10000);
+
 }
