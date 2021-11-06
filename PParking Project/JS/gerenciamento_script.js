6e7e9de04fc472fd0dd4bbc9ab0de46120fefacd
Davi Carboni <davicarlosm.carboni@gmail.com>
2021-11-05 22:57:18 -0300

    gerenciamento_fix

--- PParking Project/JS/gerenciamento_script.js
+++ PParking Project/JS/gerenciamento_script.js
@@ -1,47 +1,31 @@
-function getVagas()
-{
-    document.getElementById("titulo-busca").innerHTML = "Vagas";
-
-    /* 
-    var linha = document.createElement("tr");
-    var vagasTotais = document.createElement("td");
-    var vagasDisponiveis = document.createElement("td");
-    var vagasOcupadas = document.createElement("td");
-
-    vagasTotais.className = "tabela-dados";
-
-    var textoVagasTotais = document.createTextNode("Vagas Totais");
-    
-    vagasTotais.appendChild(textoVagasTotais);
-
-    linha.appendChild(vagasTotais);
-
-    var corpo_tabela = document.querySelector("tbody").appendChild(linha);
-
-    */
+function limpaDados(){
+    document.getElementById("titulo-dados").innerHTML = "";
 }
 
-function getEntradas()
+function getBloqueados()
 {
-    document.getElementById("titulo-busca").innerHTML = "Entradas";
+    document.getElementById("titulo-busca").innerHTML = "Bloqueados";
+    limpaDados();
+}
+
+function getDesbloqueados()
+{
+    document.getElementById("titulo-busca").innerHTML = "Desbloqueados";
+    limpaDados();
 
 }
 
-function getSaidas()
+function getEncerrados()
 {
-    document.getElementById("titulo-busca").innerHTML = "Saídas";
-
-}
-
-function getPagamentos()
-{
-    document.getElementById("titulo-busca").innerHTML = "Pagamentos efetuados";
+    document.getElementById("titulo-busca").innerHTML = "Encerrados";
+    limpaDados();
 
 }
 
 function getFaturamento()
 {
     document.getElementById("titulo-busca").innerHTML = "Registro de faturamento";
+    limpaDados();
 
 }
 
